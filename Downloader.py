ff17205b9bbc1aace8652ec498150b223b72246b
shizuku <eddy540511@yahoo.com.tw>
2017-11-30 06:26:57 +0800

    first commit

---
+++ Downloader.py
@@ -0,0 +1,29 @@
+import requests
+import re
+import json
+import sys
+from urllib.parse import parse_qs
+
+# Something like https://www.youtube.com/watch?v=XXXXXXXXXXX
+url = sys.argv[1]
+
+res = requests.get(url)
+args_json_string = re.search('ytplayer.config = ({.*?});', res.text)
+
+if len(args_json_string.groups()) is 1:
+    j = json.loads(args_json_string.group(1))
+else:
+    print('no config found')
+    sys.exit()
+
+# Take off '/' from video name      
+title = j['args']['title'].replace('/', '')
+qs = parse_qs(j['args']['url_encoded_fmt_stream_map'])
+res = requests.get(qs['url'][0])
+if res.status_code == 200:
+    # Directly replace existing file
+    with open(title+'.mp4', 'wb') as f:
+        for chunk in res:
+            f.write(chunk)
+else:
+    print('request error')
---
+++ README.md
@@ -0,0 +1,11 @@
+## Simple-Youtube-Downloader
+
+To download a video from youtube:
+
+```python
+> python Downloader.py https://www.youtube.com/watch?v=XXXXXXXXXXX
+```
+
+Type: mp4  
+Filename: Name of the video  
+Path: Where the downloader.py is
